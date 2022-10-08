bd48d8f0bc2134b517a9b968b9081582f799145e
juanpi8345 <91738384+juanpi8345@users.noreply.github.com>
2022-10-08 14:38:04 -0300

    Update registro.php

--- registro.php
+++ registro.php
@@ -1,9 +1,16 @@
 <?php
 
+require_once("config/database.php");
+date_default_timezone_set("America/Argentina/Jujuy");
+
+$database = new Database();
+$db = $database->getConnection();
+
 $nombre = $_POST["nombre"];
 $email = $_POST["email"];
 $asunto = $_POST["asunto"];
 $mensaje = $_POST["mensaje"];
+$fecha = date("y-m-d H:i:s");
 
 // if(isset($nombre) and !empty($nombre)){
 
@@ -57,6 +64,10 @@ $listaErrores = array();
 // 	$respuesta["mensaje"] = "Ingrese nombre";
 // }
 
+function is_ajax() {
+    return (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && (strtolower(getenv('HTTP_X_REQUESTED_WITH')) === 'xmlhttprequest'));
+}//Funciona si nos indica si estamos usando ajax para que no se envie informacion desde la url
+
 
 if(!isset($nombre) or empty($nombre)){
 	array_push($listaErrores, array(
@@ -92,13 +103,40 @@ if(!isset($asunto) or empty($asunto)){
 
 }
 
-if(count($listaErrores) > 0){
-	$respuesta["tipo"] = 2;
-	$respuesta["errores"] = $listaErrores;
-}else{
-	$respuesta["tipo"] = 1;
-	$respuesta["mensaje"] = "Se registro satisfactoriamente";
-}
-echo json_encode($respuesta);
+if(is_ajax()){
 
+
+
+	if(count($listaErrores) > 0){
+		$respuesta["tipo"] = 2;
+		$respuesta["errores"] = $listaErrores;
+	}else{
+
+		$declaracion = $db->prepare("INSERT INTO contacto(nombre,email,asunto,mensaje,fecha)
+			VALUES(:nombre, :email, :asunto, :mensaje, :fecha)");
+		$declaracion->bindParam(":nombre", $nombre, PDO::PARAM_STR);
+		$declaracion->bindParam(":email", $email, PDO::PARAM_STR);
+		$declaracion->bindParam(":asunto", $asunto, PDO::PARAM_STR);
+		$declaracion->bindParam(":mensaje", $mensaje, PDO::PARAM_STR);
+		$declaracion->bindParam(":fecha", $fecha, PDO::PARAM_STR);
+		$declaracion->execute();
+
+		$ultimoId = $db->lastInsertId();
+
+		if($ultimoId){
+			$respuesta["tipo"] = 1;
+			$respuesta["mensaje"] = "Se registro satisfactoriamente";
+		}else{
+			$respuesta["tipo"] = 1;
+			$respuesta["mensaje"] = "Problema de insercion";
+		}
+
+	}
+
+	}else{
+		$respuesta["tipo"] = 1;
+		$respuesta["mensaje"] = "Problema de servidor";
+	}
+
+echo json_encode($respuesta);
   ?>
