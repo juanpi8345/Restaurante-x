52b1b1c2c79b4b194e50719859423fa2d89821be
JPregliasco <91738384+juanpi8345@users.noreply.github.com>
2022-12-23 14:44:13 -0300

    Delete cache.js

--- js/cache.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-caches.open("files").then(cache=>{
-    cache.addAll(["js/funciones.js","img","css/estilos.css","css/normalize.css","index.html","nosotros.html","contacto.html"]);
-})
