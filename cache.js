7cf7c23da2e30035803e9359de2780b01f034c49
JPregliasco <91738384+juanpi8345@users.noreply.github.com>
2022-12-23 14:52:20 -0300

    Update cache.js

--- cache.js
+++ cache.js
@@ -1,5 +1,5 @@
 "use strict";
 
 caches.open("files").then(cache=>{
-    cache.addAll(["js/funciones.js","css/estilos.css","img/","css/normalize.css","index.html","nosotros.html","contacto.html"]);
+    cache.addAll(["js/funciones.js","css/estilos.css","img","css/normalize.css","index.html","nosotros.html","contacto.html"]);
 })
